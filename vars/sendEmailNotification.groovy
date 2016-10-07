f967ef457252990ade149c973184ff148029b6d2
Pedro Algarvio <pedro@algarvio.me>
2016-10-07 16:55:03 +0100

    Add sendEmailNotification

---
+++ vars/sendEmailNotification.groovy
@@ -0,0 +1,31 @@
+#!/usr/bin/env groovy
+
+
+def call(String build_status = 'SUCCESSFUL', String message) {
+    build_status = build_status ?: 'SUCCESSFUL'
+
+    // Default values
+    def color_code = '#FF0000'
+    def full_display_name = currentBuild.getFullDisplayName()
+    def subject = "${build_status}: ${full_display_name}"
+
+    // Override default values based on build status
+    if (build_status == 'UNSTABLE') {
+        color_code = '#FFFF00'
+    } else if (build_status == 'SUCCESSFUL') {
+        color_code = '#00FF00'
+    } else {
+        color_code = '#FF0000'
+    }
+
+    def body = """<p><span style="color: ${color_code}">${build_status}</span>: ${full_display_name}:</p>
+    <p>Check console output at &QUOT;<a href='${env.BUILD_URL}'>${full_display_name}</a>&QUOT;</p>"""
+
+    // Send an email notification!
+    emailext(attachLog: true,
+             body: body,
+             recipientProviders: [[$class: 'CulpritsRecipientProvider'],
+                                  [$class: 'FirstFailingBuildSuspectsRecipientProvider'],
+                                  [$class: 'RequesterRecipientProvider']],
+             subject: subject)
+}
