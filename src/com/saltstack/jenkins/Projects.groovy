542e9ab1c67c2ee44b5e329cbbb5eb3d4bcd8df5
Pedro Algarvio <pedro@algarvio.me>
2016-10-09 17:15:06 +0100

    Another port

---
+++ src/com/saltstack/jenkins/Projects.groovy
@@ -0,0 +1,94 @@
+package com.saltstack.jenkins
+
+import groovy.json.*
+import java.util.logging.Level
+import java.util.logging.Logger
+import com.saltstack.jenkins.projects.*
+import com.saltstack.jenkins.PushHooksRecorder
+import com.coravy.hudson.plugins.github.GithubProjectProperty
+
+class Projects {
+
+    private static final Logger LOGGER = Logger.getLogger(Project.class.getName());
+
+    static def get_projects() {
+        return [
+            new Raas(),
+            new Raas2(),
+            new Ubu(),
+            new Pack(),
+            new Pack2()
+        ]
+    }
+
+    static def toMap(Boolean include_branches = false, Boolean include_prs = false) {
+        def data = [:]
+        this.get_projects().each() { project ->
+            data[project.name] = project.toMap(include_branches, include_prs)
+        }
+        return data
+    }
+
+    static def toJSON(Boolean include_branches = false, Boolean include_prs = false) {
+        return new JsonBuilder(this.toMap(include_branches, include_prs)).toString()
+    }
+
+    def trigger_projects_pull_request_seed_jobs(manager) {
+        this.filterOutProject(manager.build).triggerPullRequestSeedJob(manager)
+    }
+
+    def filterOutProject(build) {
+        def match = null
+        this.get_projects().find { project ->
+            def github_project_property = build.getProject().getProperty(GithubProjectProperty.class)
+            if ( github_project_property != null ) {
+                def github_repo_url = github_project_property.getProjectUrl().toString()
+                if ( github_repo_url[-1] == '/' ) {
+                    github_repo_url = github_repo_url[0..-2]
+                }
+                if ( github_repo_url == "https://github.com/${project.repo}" ) {
+                    match = project
+                    return true
+                }
+                return false
+            } else {
+                LOGGER.log(Level.FINE, "Project ${project.getFullDisplayName()} does not have the GithubProjectProperty defined")
+            }
+            return false
+        }
+        return match
+    }
+
+    def setCommitStatusPre(currentBuild, commit_status_context, out) {
+        def project = this.filterOutProject(currentBuild)
+        if ( project.set_commit_status ) {
+            project.setCommitStatusPre(currentBuild, commit_status_context, out)
+        } else {
+            out.println "Setting commit status for project ${project.display_name} is disabled. Skipping..."
+        }
+    }
+
+    def setCommitStatusPost(manager) {
+        def project = this.filterOutProject(manager.build)
+        if ( project.set_commit_status ) {
+            project.setCommitStatusPost(manager)
+        } else {
+            manager.listener.logger.println "Setting commit status for project ${project.display_name} is disabled. Skipping..."
+        }
+    }
+
+    def triggerPullRequestJobs(manager) {
+        def match = this.filterOutProject(manager.build)
+        if ( match != null ) {
+            match.triggerPullRequestJobs(manager)
+        }
+    }
+
+    def cleanOldPullRequests(manager, howOld=7) {
+        this.filterOutProject(manager.build).cleanOldPullRequests(manager, howOld)
+    }
+
+    def addBuildSummary(manager) {
+        this.filterOutProject(manager.build).addBuildSummary(manager)
+    }
+}
