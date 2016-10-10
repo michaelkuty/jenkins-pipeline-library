203cf47062373f2e8c1c9ff0cbedf5922ed4bae9
Pedro Algarvio <pedro@algarvio.me>
2016-10-10 11:44:18 +0100

    Mapping

--- vars/getSaltstackProjects.groovy
+++ vars/getSaltstackProjects.groovy
@@ -5,5 +5,5 @@ def call(Map options) {
     options = options ?: [:]
     def include_branches = options.get("include_branches", false)
     def include_prs = options.get("include_prs")
-    return Projects.toJSON(include_branches = true, include_prs = false)
+    return Projects.toMap(include_branches = true, include_prs = false)
 }
