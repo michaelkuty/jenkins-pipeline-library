a7d9952f8ba4008ec6c8c689ded65706d2f5ab6f
Pedro Algarvio <pedro@algarvio.me>
2016-10-12 16:59:22 +0100

    Follow the same approach as slack notification

--- vars/sendEmailNotification.groovy
+++ vars/sendEmailNotification.groovy
@@ -1,23 +1,37 @@
 #!/usr/bin/env groovy
 
 
-def call(String build_status = 'SUCCESSFUL') {
-    build_status = build_status ?: 'SUCCESSFUL'
+def call() {
+    def color_name
+    def color_code
+    def build_status
 
-    // Default values
-    def color_code = '#FF0000'
-    def full_display_name = currentBuild.getFullDisplayName()
-    def subject = "${build_status}: ${full_display_name}"
+    def result = currentBuild.getResult()
 
-    // Override default values based on build status
-    if (build_status == 'UNSTABLE') {
+    if (result == null) { // Build is ongoing
+        build_status = 'In Progress'
+        color_name = 'YELLOW'
         color_code = '#FFFF00'
-    } else if (build_status == 'SUCCESSFUL') {
+    } else if (result.isBetterOrEqualTo(Result.SUCCESS)) {
+        build_status = 'Success'
+        color_name = 'GREEN'
         color_code = '#00FF00'
+    } else if (result.isBetterOrEqualTo(Result.FAILURE)) {
+        build_status = 'Failed'
+        color_name = 'RED'
+        color_code = '#FF0000'
+    } else if (result.isBetterOrEqualTo(Result.UNSTABLE)) {
+        build_status = 'Unstable'
+        color_name = 'YELLOW'
+        color_code = '#FFFF00'
     } else {
+        build_status = 'Error'
+        color_name = 'RED'
         color_code = '#FF0000'
     }
 
+    def full_display_name = currentBuild.getFullDisplayName()
+    def subject = "${build_status}: ${full_display_name}"
     def body = """<p><span style="color: ${color_code}">${build_status}</span>: ${full_display_name}:</p>
     <p>Check console output at &QUOT;<a href='${env.BUILD_URL}'>${full_display_name}</a>&QUOT;</p>"""
 
