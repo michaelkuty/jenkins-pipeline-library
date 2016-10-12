707156d58f04fc6574ce5cf43474da2c1f09a37b
Pedro Algarvio <pedro@algarvio.me>
2016-10-12 16:52:44 +0100

    Smarten up sendSlackNotification

--- vars/sendSlackNotification.groovy
+++ vars/sendSlackNotification.groovy
@@ -1,24 +1,34 @@
 #!/usr/bin/env groovy
 
 // https://github.com/jenkinsci/slack-plugin/blob/master/src/main/java/jenkins/plugins/slack/ActiveNotifier.java
+import hudson.model.Result
 
-def call(String build_status = 'SUCCESSFUL', String channel = null) {
-    build_status = build_status ?: 'SUCCESSFUL'
 
-    // Default values
-    def color_name = 'RED'
-    def color_code = '#FF0000'
-    def full_display_name = currentBuild.getFullDisplayName()
-    def summary = "*${build_status}*:\n  <${env.BUILD_URL}|${full_display_name}>"
+def call(String channel = null) {
+    def color_name
+    def color_code
+    def build_status
 
-    // Override default values based on build status
-    if (build_status == 'UNSTABLE') {
+    def result = currentBuild.getResult()
+
+    if (result == null) { // Build is ongoing
+        build_status = 'In Progress'
         color_name = 'YELLOW'
         color_code = '#FFFF00'
-    } else if (build_status == 'SUCCESSFUL') {
+    } else if (result.isBetterOrEqualTo(Result.SUCCESS)) {
+        build_status = 'Success'
         color_name = 'GREEN'
         color_code = '#00FF00'
+    } else if (result.isBetterOrEqualTo(Result.FAILURE)) {
+        build_status = 'Failed'
+        color_name = 'RED'
+        color_code = '#FF0000'
+    } else if (result.isBetterOrEqualTo(Result.UNSTABLE)) {
+        build_status = 'Unstable'
+        color_name = 'YELLOW'
+        color_code = '#FFFF00'
     } else {
+        build_status = 'Error'
         color_name = 'RED'
         color_code = '#FF0000'
     }
@@ -27,6 +37,9 @@ def call(String build_status = 'SUCCESSFUL', String channel = null) {
         // Make sure channel starts with a #
         channel = (channel.startsWith('#')) ? channel : "#${channel}"
     }
+
+    def full_display_name = currentBuild.getFullDisplayName()
+    def summary = "*${build_status}*:\n  <${env.BUILD_URL}|${full_display_name}>"
 
     // Send a slack notification!
     slackSend(channel: channel, color: color_code, message: summary)
