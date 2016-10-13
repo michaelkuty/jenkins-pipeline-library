44b23d384a925f472761879fc3d7a8927982437a
Pedro Algarvio <pedro@algarvio.me>
2016-10-13 16:54:17 +0100

    Scoping

--- vars/getRepoBranchLatestCommit.groovy
+++ vars/getRepoBranchLatestCommit.groovy
@@ -4,9 +4,9 @@ import org.kohsuke.github.GitHub
 def call(String credentialsId, String repo, String branch) {
     withGithubToken(credentialsId) {
         def gh = GitHub.connectUsingOAuth(env.GITHUB_TOKEN)
-        def repo = gh.getRepository(repo)
-        def branch = repo.getBranch(branch)
-        def sha1 = "${branch.getSHA1()}"
+        def _repo = gh.getRepository(repo)
+        def _branch = _repo.getBranch(branch)
+        def sha1 = "${_branch.getSHA1()}"
         return sha1
     }
 }
