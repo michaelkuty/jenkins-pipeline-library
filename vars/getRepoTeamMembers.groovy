c61eb5cdf153897695b0a4b6c8ef0a91c6228e1e
Pedro Algarvio <pedro@algarvio.me>
2016-10-14 18:52:52 +0100

    Add vars/getRepoTeamMembers.groovy

---
+++ vars/getRepoTeamMembers.groovy
@@ -0,0 +1,19 @@
+import org.kohsuke.github.GitHub
+
+@NonCPS
+def call(String credentialsId, String repo, String team_name) {
+    withGithubToken(credentialsId) {
+        def gh = GitHub.connectUsingOAuth(env.GITHUB_TOKEN)
+        def _repo = gh.getRepository(repo)
+        def teams = repo.getTeams()
+        def usernames = []
+        teams.each { team ->
+            if ( team.getName() == team_name ) {
+                team.getMembers().each { user ->
+                    usernames.add(user.login)
+                }
+            }
+        }
+        return usernames
+    }
+}
