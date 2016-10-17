28d5da34eab115345023f5e92ffec29c88a3a07f
Pedro Algarvio <pedro@algarvio.me>
2016-10-17 13:39:20 +0100

    Add vars/commitContextWrappedStage.groovy

---
+++ vars/commitContextWrappedStage.groovy
@@ -0,0 +1,12 @@
+
+
+def call(String title, String commit_context, Closure body=null) {
+    try {
+        setGitHubPullRequestStatus state: 'PENDING', context: commit_context, message: "${title} - Started"
+        if (body) { body() }
+        setGitHubPullRequestStatus state: 'SUCCESS', context: commit_context, message: "${title} - Success"
+    } catch(e) {
+        setGitHubPullRequestStatus state: 'ERROR', context: commit_context, message: "${title} - Error"
+        throw e
+    }
+}
