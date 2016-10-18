5f9d812393a46d4af60e8ab43204e44d05be9b1f
Pedro Algarvio <pedro@algarvio.me>
2016-10-18 13:04:16 +0100

    Revert "Don't let the commit status step make the build fail"
    This reverts commit 2040f889c9d8c390f2392b7c8c8521df89e875f8.

--- vars/commitContextWrappedStage.groovy
+++ vars/commitContextWrappedStage.groovy
@@ -3,47 +3,50 @@
 @NonCPS
 def call(String title, String commit_context, Closure body=null) {
     try {
-        try {
-            step([$class: 'GitHubCommitStatusSetter',
-                  reposSource: [$class: 'ManuallyEnteredRepositorySource', url: getGitHubProjectProperty()],
-                  contextSource: [$class: 'ManuallyEnteredCommitContextSource', context: commit_context],
-                  errorHandlers: [[$class: 'ShallowAnyErrorHandler']],
-                  statusResultSource: [
-                    $class: 'ConditionalStatusResultSource',
-                    results: [[$class: 'AnyBuildResult', message: "${title} - Build ${env.BUILD_NUMBER} Started", state: 'PENDING']]
-                  ]
-            ])
-        } catch(e) {}
+        step([$class: 'GitHubCommitStatusSetter',
+              reposSource: [$class: 'ManuallyEnteredRepositorySource', url: getGitHubProjectProperty()],
+              contextSource: [$class: 'ManuallyEnteredCommitContextSource', context: commit_context],
+              errorHandlers: [[$class: 'ShallowAnyErrorHandler']],
+              statusResultSource: [
+                $class: 'ConditionalStatusResultSource',
+                results: [[$class: 'AnyBuildResult', message: "${title} - Build ${env.BUILD_NUMBER} Started", state: 'PENDING']]
+              ]
+        ])
         if (body) { body() }
-        try {
-            step([$class: 'GitHubCommitStatusSetter',
-                  reposSource: [$class: 'ManuallyEnteredRepositorySource', url: getGitHubProjectProperty()],
-                  contextSource: [$class: 'ManuallyEnteredCommitContextSource', context: commit_context],
-                  errorHandlers: [[$class: 'ShallowAnyErrorHandler']],
-                  statusResultSource: [
-                    $class: 'ConditionalStatusResultSource',
-                    results: [
-                        [$class: 'BetterThanOrEqualBuildResult', message: "${title} - Build ${env.BUILD_NUMBER} Succeeded", result: 'SUCCESS', state: 'SUCCESS'],
-                    ]
-                  ]
-                ])
-            } catch(e) {}
+        step([$class: 'GitHubCommitStatusSetter',
+              reposSource: [$class: 'ManuallyEnteredRepositorySource', url: getGitHubProjectProperty()],
+              contextSource: [$class: 'ManuallyEnteredCommitContextSource', context: commit_context],
+              errorHandlers: [[$class: 'ShallowAnyErrorHandler']],
+              statusResultSource: [
+                $class: 'ConditionalStatusResultSource',
+                results: [[$class: 'BetterThanOrEqualBuildResult', message: "${title} - Build ${env.BUILD_NUMBER} Started", state: 'PENDING']]
+              ]
+        ])
+        step([$class: 'GitHubCommitStatusSetter',
+              reposSource: [$class: 'ManuallyEnteredRepositorySource', url: getGitHubProjectProperty()],
+              contextSource: [$class: 'ManuallyEnteredCommitContextSource', context: commit_context],
+              errorHandlers: [[$class: 'ShallowAnyErrorHandler']],
+              statusResultSource: [
+                $class: 'ConditionalStatusResultSource',
+                results: [
+                    [$class: 'BetterThanOrEqualBuildResult', message: "${title} - Build ${env.BUILD_NUMBER} Succeeded", result: 'SUCCESS', state: 'SUCCESS'],
+                ]
+              ]
+            ])
     } catch(e) {
         currentBuild.result = 'FAILED'
-        try {
-            step([$class: 'GitHubCommitStatusSetter',
-                  reposSource: [$class: 'ManuallyEnteredRepositorySource', url: getGitHubProjectProperty()],
-                  contextSource: [$class: 'ManuallyEnteredCommitContextSource', context: commit_context],
-                  errorHandlers: [[$class: 'ShallowAnyErrorHandler']],
-                  statusResultSource: [
-                    $class: 'ConditionalStatusResultSource',
-                    results: [
-                        [$class: 'BetterThanOrEqualBuildResult', message: "${title} - Build ${env.BUILD_NUMBER} Unstable", result: 'UNSTABLE', state: 'FAILURE'],
-                        [$class: 'BetterThanOrEqualBuildResult', message: "${title} - Build ${env.BUILD_NUMBER} Error", result: 'FAILURE', state: 'ERROR']
-                    ]
-                  ]
-                ])
-            } catch(e) {}
+        step([$class: 'GitHubCommitStatusSetter',
+              reposSource: [$class: 'ManuallyEnteredRepositorySource', url: getGitHubProjectProperty()],
+              contextSource: [$class: 'ManuallyEnteredCommitContextSource', context: commit_context],
+              errorHandlers: [[$class: 'ShallowAnyErrorHandler']],
+              statusResultSource: [
+                $class: 'ConditionalStatusResultSource',
+                results: [
+                    [$class: 'BetterThanOrEqualBuildResult', message: "${title} - Build ${env.BUILD_NUMBER} Unstable", result: 'UNSTABLE', state: 'FAILURE'],
+                    [$class: 'BetterThanOrEqualBuildResult', message: "${title} - Build ${env.BUILD_NUMBER} Error", result: 'FAILURE', state: 'ERROR']
+                ]
+              ]
+            ])
         throw e
     }
 }
