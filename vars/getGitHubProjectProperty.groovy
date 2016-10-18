ad5d957f0657edcc0d1b10d8fe155021e72b09e6
Pedro Algarvio <pedro@algarvio.me>
2016-10-18 12:32:28 +0100

    Like this?!

--- vars/getGitHubProjectProperty.groovy
+++ vars/getGitHubProjectProperty.groovy
@@ -4,5 +4,5 @@ import com.coravy.hudson.plugins.github.GithubProjectProperty
 
 @NonCPS
 def call() {
-    return currentBuild.rawBuild.getParent().getProject().getProperty(GithubProjectProperty.class)
+    return currentBuild.rawBuild.getParent().getProperty(GithubProjectProperty.class)
 }
