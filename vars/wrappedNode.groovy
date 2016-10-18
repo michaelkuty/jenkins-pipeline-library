0e9c0a201bebfba12b7352c40a536f2612239c3d
Pedro Algarvio <pedro@algarvio.me>
2016-10-18 15:44:15 +0100

    Allow providing a global timeout

--- vars/wrappedNode.groovy
+++ vars/wrappedNode.groovy
@@ -3,7 +3,33 @@ def call(Map vars, Closure body=null) {
     node(vars.get("label", null)) {
         wrap([$class: 'TimestamperBuildWrapper']) {
             wrap([$class: 'AnsiColorBuildWrapper']) {
-                if (body) { body() }
+                currentBuild.result = 'PENDING'
+                timeout_vars = vars.get('timeout', null)
+                if ( timeout_vars )
+                    timeout(time: timeout_vars.get('time'), unit: timeout_vars.get('unit', 'MINUTES')) {
+                        try {
+                            if (body) { body() }
+                            currentBuild.result = 'SUCCESS'
+                        } catch(InterruptedException ie) {
+                            currentBuild.result = 'ABORTED'
+                            throw e
+                        } catch(e) {
+                            currentBuild.result = 'FAILURE'
+                            throw e
+                        }
+                    }
+                } else {
+                    try {
+                        if (body) { body() }
+                        currentBuild.result = 'SUCCESS'
+                    } catch(InterruptedException ie) {
+                        currentBuild.result = 'ABORTED'
+                        throw e
+                    } catch(e) {
+                        currentBuild.result = 'FAILURE'
+                        throw e
+                    }
+                }
             }
         }
     }
