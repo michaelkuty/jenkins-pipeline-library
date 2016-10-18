5da3be344975b3526d3c148302e1607a0770c31e
Pedro Algarvio <pedro@algarvio.me>
2016-10-18 13:05:02 +0100

    Revert "Switch to another commit status setting approach"
    This reverts commit 0f24c5e88dd699b5bdccaea657251c9ae47e1969.

--- vars/commitContextWrappedStage.groovy
+++ vars/commitContextWrappedStage.groovy
@@ -1,44 +1,12 @@
-#!/usr/bin/env groovy
 
-@NonCPS
+
 def call(String title, String commit_context, Closure body=null) {
     try {
-        step([$class: 'GitHubCommitStatusSetter',
-              contextSource: [$class: 'ManuallyEnteredCommitContextSource', context: commit_context],
-              errorHandlers: [[$class: 'ShallowAnyErrorHandler']],
-              statusResultSource: [$class: 'ConditionalStatusResultSource',
-              results: [[$class: 'AnyBuildResult', message: "${title} - Build ${env.BUILD_NUMBER} Started", state: 'PENDING']]]
-        ])
+        setGitHubPullRequestStatus state: 'PENDING', context: commit_context, message: "${title} - Started"
         if (body) { body() }
-        step([$class: 'GitHubCommitStatusSetter',
-              contextSource: [$class: 'ManuallyEnteredCommitContextSource', context: commit_context],
-              errorHandlers: [[$class: 'ShallowAnyErrorHandler']],
-              statusResultSource: [$class: 'ConditionalStatusResultSource',
-              results: [[$class: 'BetterThanOrEqualBuildResult', message: "${title} - Build ${env.BUILD_NUMBER} Started", state: 'PENDING']]]
-        ])
-        step([$class: 'GitHubCommitStatusSetter',
-              contextSource: [$class: 'ManuallyEnteredCommitContextSource', context: commit_context],
-              errorHandlers: [[$class: 'ShallowAnyErrorHandler']],
-              statusResultSource: [
-                $class: 'ConditionalStatusResultSource',
-                results: [
-                    [$class: 'BetterThanOrEqualBuildResult', message: "${title} - Build ${env.BUILD_NUMBER} Succeeded", result: 'SUCCESS', state: 'SUCCESS'],
-                ]
-              ]
-            ])
+        setGitHubPullRequestStatus state: 'SUCCESS', context: commit_context, message: "${title} - Success"
     } catch(e) {
-        currentBuild.result = 'FAILED'
-        step([$class: 'GitHubCommitStatusSetter',
-              contextSource: [$class: 'ManuallyEnteredCommitContextSource', context: commit_context],
-              errorHandlers: [[$class: 'ShallowAnyErrorHandler']],
-              statusResultSource: [
-                $class: 'ConditionalStatusResultSource',
-                results: [
-                    [$class: 'BetterThanOrEqualBuildResult', message: "${title} - Build ${env.BUILD_NUMBER} Unstable", result: 'UNSTABLE', state: 'FAILURE'],
-                    [$class: 'BetterThanOrEqualBuildResult', message: "${title} - Build ${env.BUILD_NUMBER} Error", result: 'FAILURE', state: 'ERROR']
-                ]
-              ]
-            ])
+        setGitHubPullRequestStatus state: 'ERROR', context: commit_context, message: "${title} - Error"
         throw e
     }
 }
