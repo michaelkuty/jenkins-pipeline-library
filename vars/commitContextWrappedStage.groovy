06b50c5566fe755add4d14c4ec874e5ff5c3469f
Pedro Algarvio <pedro@algarvio.me>
2016-10-18 13:03:59 +0100

    Revert "Allow skipping the pending state"
    This reverts commit 45d8e69e8c6b31b828d96e233b4416be6db4602a.

--- vars/commitContextWrappedStage.groovy
+++ vars/commitContextWrappedStage.groovy
@@ -1,21 +1,19 @@
 #!/usr/bin/env groovy
 
 @NonCPS
-def call(String title, String commit_context, Boolean set_pending=true, Closure body=null) {
+def call(String title, String commit_context, Closure body=null) {
     try {
-        if ( set_pending ) {
-            try {
-                step([$class: 'GitHubCommitStatusSetter',
-                      reposSource: [$class: 'ManuallyEnteredRepositorySource', url: getGitHubProjectProperty()],
-                      contextSource: [$class: 'ManuallyEnteredCommitContextSource', context: commit_context],
-                      errorHandlers: [[$class: 'ShallowAnyErrorHandler']],
-                      statusResultSource: [
-                        $class: 'ConditionalStatusResultSource',
-                        results: [[$class: 'AnyBuildResult', message: "${title} - Build ${env.BUILD_NUMBER} Started", state: 'PENDING']]
-                      ]
-                ])
-            } catch(ee1) {}
-        }
+        try {
+            step([$class: 'GitHubCommitStatusSetter',
+                  reposSource: [$class: 'ManuallyEnteredRepositorySource', url: getGitHubProjectProperty()],
+                  contextSource: [$class: 'ManuallyEnteredCommitContextSource', context: commit_context],
+                  errorHandlers: [[$class: 'ShallowAnyErrorHandler']],
+                  statusResultSource: [
+                    $class: 'ConditionalStatusResultSource',
+                    results: [[$class: 'AnyBuildResult', message: "${title} - Build ${env.BUILD_NUMBER} Started", state: 'PENDING']]
+                  ]
+            ])
+        } catch(ee1) {}
         if (body) { body() }
         try {
             step([$class: 'GitHubCommitStatusSetter',
