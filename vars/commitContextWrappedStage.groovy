1e43f4e2a913c6d0640e788b667be4cd36f8a060
Pedro Algarvio <pedro@algarvio.me>
2016-10-18 12:19:23 +0100

    Use the GH project property

--- vars/commitContextWrappedStage.groovy
+++ vars/commitContextWrappedStage.groovy
@@ -4,19 +4,26 @@
 def call(String title, String commit_context, Closure body=null) {
     try {
         step([$class: 'GitHubCommitStatusSetter',
+              reposSource: [$class: 'ManuallyEnteredRepositorySource', url: getGitHubProjectProperty()],
               contextSource: [$class: 'ManuallyEnteredCommitContextSource', context: commit_context],
               errorHandlers: [[$class: 'ShallowAnyErrorHandler']],
-              statusResultSource: [$class: 'ConditionalStatusResultSource',
-              results: [[$class: 'AnyBuildResult', message: "${title} - Build ${env.BUILD_NUMBER} Started", state: 'PENDING']]]
+              statusResultSource: [
+                $class: 'ConditionalStatusResultSource',
+                results: [[$class: 'AnyBuildResult', message: "${title} - Build ${env.BUILD_NUMBER} Started", state: 'PENDING']]
+              ]
         ])
         if (body) { body() }
         step([$class: 'GitHubCommitStatusSetter',
+              reposSource: [$class: 'ManuallyEnteredRepositorySource', url: getGitHubProjectProperty()],
               contextSource: [$class: 'ManuallyEnteredCommitContextSource', context: commit_context],
               errorHandlers: [[$class: 'ShallowAnyErrorHandler']],
-              statusResultSource: [$class: 'ConditionalStatusResultSource',
-              results: [[$class: 'BetterThanOrEqualBuildResult', message: "${title} - Build ${env.BUILD_NUMBER} Started", state: 'PENDING']]]
+              statusResultSource: [
+                $class: 'ConditionalStatusResultSource',
+                results: [[$class: 'BetterThanOrEqualBuildResult', message: "${title} - Build ${env.BUILD_NUMBER} Started", state: 'PENDING']]
+              ]
         ])
         step([$class: 'GitHubCommitStatusSetter',
+              reposSource: [$class: 'ManuallyEnteredRepositorySource', url: getGitHubProjectProperty()],
               contextSource: [$class: 'ManuallyEnteredCommitContextSource', context: commit_context],
               errorHandlers: [[$class: 'ShallowAnyErrorHandler']],
               statusResultSource: [
@@ -29,6 +36,7 @@ def call(String title, String commit_context, Closure body=null) {
     } catch(e) {
         currentBuild.result = 'FAILED'
         step([$class: 'GitHubCommitStatusSetter',
+              reposSource: [$class: 'ManuallyEnteredRepositorySource', url: getGitHubProjectProperty()],
               contextSource: [$class: 'ManuallyEnteredCommitContextSource', context: commit_context],
               errorHandlers: [[$class: 'ShallowAnyErrorHandler']],
               statusResultSource: [
