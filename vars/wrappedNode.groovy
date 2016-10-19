251978a76158fb2802a7f95dc3a679a0e11d2c4a
Pedro Algarvio <pedro@algarvio.me>
2016-10-19 01:02:53 +0100

    Explain the build results being set

--- vars/wrappedNode.groovy
+++ vars/wrappedNode.groovy
@@ -10,11 +10,14 @@ def call(Map vars, Closure body=null) {
                         try {
                             if (body) { body() }
                             currentBuild.result = 'SUCCESS'
+                            echo "Setting currentBuild.result to ${currentBuild.result}"
                         } catch(InterruptedException ie) {
                             currentBuild.result = 'ABORTED'
+                            echo "Setting currentBuild.result to ${currentBuild.result}"
                             throw e
                         } catch(e) {
                             currentBuild.result = 'FAILURE'
+                            echo "Setting currentBuild.result to ${currentBuild.result}"
                             throw e
                         }
                     }
@@ -22,11 +25,14 @@ def call(Map vars, Closure body=null) {
                     try {
                         if (body) { body() }
                         currentBuild.result = 'SUCCESS'
+                        echo "Setting currentBuild.result to ${currentBuild.result}"
                     } catch(InterruptedException ie) {
                         currentBuild.result = 'ABORTED'
+                        echo "Setting currentBuild.result to ${currentBuild.result}"
                         throw e
                     } catch(e) {
                         currentBuild.result = 'FAILURE'
+                        echo "Setting currentBuild.result to ${currentBuild.result}"
                         throw e
                     }
                 }
