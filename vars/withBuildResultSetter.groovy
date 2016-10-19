5f2c1f35328737f31b5211e00e9d308299a7ac1b
Pedro Algarvio <pedro@algarvio.me>
2016-10-19 02:04:28 +0100

    Add vars/withBuildResultSetter.groovy

---
+++ vars/withBuildResultSetter.groovy
@@ -0,0 +1,15 @@
+def call(Closure body=null) {
+    try {
+        if (body) { body() }
+        currentBuild.result = 'SUCCESS'
+        echo "Setting currentBuild.result to ${currentBuild.result}"
+    } catch(InterruptedException ie) {
+        currentBuild.result = 'ABORTED'
+        echo "Setting currentBuild.result to ${currentBuild.result}"
+        throw e
+    } catch(e) {
+        currentBuild.result = 'FAILURE'
+        echo "Setting currentBuild.result to ${currentBuild.result}"
+        throw e
+    }
+}
