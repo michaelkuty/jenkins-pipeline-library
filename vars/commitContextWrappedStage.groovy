684054201470a261aaba96ec2aed09d0ca069d03
Pedro Algarvio <pedro@algarvio.me>
2016-11-07 12:24:01 +0000

    Do set a stage!

--- vars/commitContextWrappedStage.groovy
+++ vars/commitContextWrappedStage.groovy
@@ -1,12 +1,14 @@
 #!/usr/bin/env groovy
 
 def call(String title, String commit_context, Closure body=null) {
-    try {
-        setGitHubPullRequestStatus state: 'PENDING', context: commit_context, message: "${title} - Started"
-        if (body) { body() }
-        setGitHubPullRequestStatus state: 'SUCCESS', context: commit_context, message: "${title} - Success"
-    } catch(e) {
-        setGitHubPullRequestStatus state: 'ERROR', context: commit_context, message: "${title} - Error"
-        throw e
+    stage(title) {
+        try {
+            setGitHubPullRequestStatus state: 'PENDING', context: commit_context, message: "${title} - Started"
+            if (body) { body() }
+            setGitHubPullRequestStatus state: 'SUCCESS', context: commit_context, message: "${title} - Success"
+        } catch(e) {
+            setGitHubPullRequestStatus state: 'ERROR', context: commit_context, message: "${title} - Error"
+            throw e
+        }
     }
 }
